f4f0b40d9e3b4e8faaa3c067a30307ba1161737f
prince3103 <prince.sethi@trantorinc.com>
2020-01-22 15:42:01 +0530

    Updated functionality for email_address and password cannot be left blank

--- resources/user.py
+++ resources/user.py
@@ -17,6 +17,10 @@ class UserRegister(Resource):
 
     def post(self):
         data = UserRegister.parser.parse_args()
+        if data['email_address']=="":
+            return {"message": "Email Address cannot be blank"}, 400
+        if data['password']=="":
+            return {"message": "Password cannot be blank"}, 400    
 
         if UserModel.find_by_email_address(data['email_address']):
             return {"message": "A user with that email_address already exists"}, 400
